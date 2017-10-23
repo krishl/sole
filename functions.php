bbedc565ee542d25fbcb62a425a6a53e971803da
github username <krishel.lasam@gmail.com>
2017-10-22 20:32:20 -0400

    place sole_dequeue_styles under hooks

--- functions.php
+++ functions.php
@@ -1,15 +1,15 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'sole_dequeue_styles', 999 );
-function sole_dequeue_styles() {
-    wp_dequeue_style( 'storefront-style' );
-    wp_dequeue_style( 'storefront-woocommerce-style' );
-}
 
 // Setup
 
 // Includes
 
 // Hooks
+add_action( 'wp_enqueue_scripts', 'sole_dequeue_styles', 999 );
+function sole_dequeue_styles() {
+    wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-woocommerce-style' );
+}
 
 // Shortcodes
 
