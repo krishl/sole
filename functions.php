36046b047028398ef6e8d633cb47aee218e7a8bd
github username <krishel.lasam@gmail.com>
2017-10-25 01:21:42 -0400

    add custom footer credit

--- functions.php
+++ functions.php
@@ -5,10 +5,18 @@
 // Includes
 
 // Hooks
-add_action( 'wp_enqueue_scripts', 'sole_dequeue_styles', 999 );
-function sole_dequeue_styles() {
-    wp_dequeue_style( 'storefront-style' );
-    wp_dequeue_style( 'storefront-woocommerce-style' );
+add_action( 'init', 'custom_remove_footer_credit', 10 );
+function custom_remove_footer_credit () {
+    remove_action( 'storefront_footer', 'storefront_credit', 20 );
+    add_action( 'storefront_footer', 'custom_storefront_credit', 20 );
+} 
+
+function custom_storefront_credit() {
+	?>
+	<div class="site-info">
+		&copy; <?php echo get_bloginfo( 'name' ) . ' ' . date( 'Y' ); ?>
+	</div>
+	<?php
 }
 
 // Shortcodes
