4eca1ab545ff22a7daad6cdb621df1fa7ba99ca3
github username <krishel.lasam@gmail.com>
2017-11-02 05:22:01 -0400

    only add border to indiviudual product page

--- functions.php
+++ functions.php
@@ -22,6 +22,21 @@ function custom_storefront_credit() {
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 // remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_show_product_sale_flash', 17 );
+add_action( 'woocommerce_before_single_product', 'open_individual_product', 5 );
+add_action( 'woocommerce_after_single_product_summary', 'close_individual_product', 10 );
+
+function open_individual_product() {
+	?>
+	<div class="individual_product">
+	<?php
+}
+
+function close_individual_product() {
+	?>
+	</div>
+	<?php
+}
+
 
 // Shortcodes
 
