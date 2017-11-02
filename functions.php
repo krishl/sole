267745ee7540a22b5c457f69b12ddb92656a2703
github username <krishel.lasam@gmail.com>
2017-11-02 04:45:17 -0400

    reposition sale button

--- functions.php
+++ functions.php
@@ -19,6 +19,10 @@ function custom_storefront_credit() {
 	<?php
 }
 
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
+// remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_show_product_sale_flash', 17 );
+
 // Shortcodes
 
 ?>
